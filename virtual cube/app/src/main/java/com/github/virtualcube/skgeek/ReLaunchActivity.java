7e5256370e54eda7da81d20efae32a24ad530d35
sketchware geek <abas93931@gmail.com>
2022-04-11 06:29:31 +0300

    Add files via upload

---
+++ virtual cube/app/src/main/java/com/github/virtualcube/skgeek/ReLaunchActivity.java
@@ -0,0 +1,109 @@
+package com.ahmed.virtualcube.schoolproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.annotation.*;
+import android.app.*;
+import android.os.*;
+import android.view.*;
+import android.view.View.*;
+import android.widget.*;
+import android.content.*;
+import android.content.res.*;
+import android.graphics.*;
+import android.graphics.drawable.*;
+import android.media.*;
+import android.net.*;
+import android.text.*;
+import android.text.style.*;
+import android.util.*;
+import android.webkit.*;
+import android.animation.*;
+import android.view.animation.*;
+import java.io.*;
+import java.util.*;
+import java.util.regex.*;
+import java.text.*;
+import org.json.*;
+import android.content.Intent;
+import android.net.Uri;
+import com.catalinjurjiu.animcubeandroid.*;
+import com.rm.rmswitch.*;
+import com.shashank.sony.fancytoastlib.*;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.DialogFragment;
+
+public class ReLaunchActivity extends AppCompatActivity {
+	
+	private Intent i = new Intent();
+	
+	@Override
+	protected void onCreate(Bundle _savedInstanceState) {
+		super.onCreate(_savedInstanceState);
+		setContentView(R.layout.re_launch);
+		initialize(_savedInstanceState);
+		initializeLogic();
+	}
+	
+	private void initialize(Bundle _savedInstanceState) {
+	}
+	
+	private void initializeLogic() {
+		i.setAction(Intent.ACTION_VIEW);
+		i.setClass(getApplicationContext(), MainActivity.class);
+		startActivity(i);
+		finish();
+	}
+	
+	
+	@Deprecated
+	public void showMessage(String _s) {
+		Toast.makeText(getApplicationContext(), _s, Toast.LENGTH_SHORT).show();
+	}
+	
+	@Deprecated
+	public int getLocationX(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[0];
+	}
+	
+	@Deprecated
+	public int getLocationY(View _v) {
+		int _location[] = new int[2];
+		_v.getLocationInWindow(_location);
+		return _location[1];
+	}
+	
+	@Deprecated
+	public int getRandom(int _min, int _max) {
+		Random random = new Random();
+		return random.nextInt(_max - _min + 1) + _min;
+	}
+	
+	@Deprecated
+	public ArrayList<Double> getCheckedItemPositionsToArray(ListView _list) {
+		ArrayList<Double> _result = new ArrayList<Double>();
+		SparseBooleanArray _arr = _list.getCheckedItemPositions();
+		for (int _iIdx = 0; _iIdx < _arr.size(); _iIdx++) {
+			if (_arr.valueAt(_iIdx))
+			_result.add((double)_arr.keyAt(_iIdx));
+		}
+		return _result;
+	}
+	
+	@Deprecated
+	public float getDip(int _input) {
+		return TypedValue.applyDimension(TypedValue.COMPLEX_UNIT_DIP, _input, getResources().getDisplayMetrics());
+	}
+	
+	@Deprecated
+	public int getDisplayWidthPixels() {
+		return getResources().getDisplayMetrics().widthPixels;
+	}
+	
+	@Deprecated
+	public int getDisplayHeightPixels() {
+		return getResources().getDisplayMetrics().heightPixels;
+	}
+}
